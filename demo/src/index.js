720e6182838dc39eba1e641a4acaf6676d972685
Revanth Kumar <revanth0212@gmail.com>
2019-12-03 11:06:08 -0600

    Initial.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,19 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import Example from "../../src";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>sample-venia-loader-icon Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>sample-venia-loader-icon Demo</h1>
+        <Example width="64" loading>
+          {"Hey"}
+        </Example>
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React, {Component} from 'react'
+import React from "react";
+import { BounceLoader } from "react-spinners";
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
+export default function(props) {
+  const { width, children } = props;
+  return (
+    <div>
+      <BounceLoader color="#FF9100" size={width} />
+      <span>{children}</span>
     </div>
-  }
+  );
 }
